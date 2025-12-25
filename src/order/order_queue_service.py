1cf334c3aafae8b6b0dacfb5f924d612e6eea188
Lin Kuai <guilin0927@gmail.com>
2025-12-25 14:16:54 -0600

    switch to fanout exchange

--- src/order/order_queue_service.py
+++ src/order/order_queue_service.py
@@ -6,7 +6,6 @@ class OrderQueueService:
         self.channel = channel
 
     async def addPlacedOrder(self, order_id: str, product_id: str, count: int):
-            queue_name = "order_queue"
-            await self.channel.declare_queue(queue_name)
+            order_exchange = await self.channel.declare_exchange("order", aio_pika.ExchangeType.FANOUT)
             data=json.dumps({ "order_id": order_id, "product_id": product_id, "count": count})
-            await self.channel.default_exchange.publish(aio_pika.Message(body=data.encode()), routing_key=queue_name)
+            await order_exchange.publish(aio_pika.Message(body=data.encode()), routing_key="")
